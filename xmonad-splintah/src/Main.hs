d99c8ae619f910122b7a8eed7055275269ab1014
Splinter Suidman <splinter@mannenopdemaan.nl>
2019-10-31 21:50:02 +0100

    Move colours to Colours module.

--- xmonad-splintah/src/Main.hs
+++ xmonad-splintah/src/Main.hs
@@ -4,6 +4,7 @@
 
 module Main where
 
+import           Colours
 import qualified Data.Map                     as Map
 import           Foreign
 import           Foreign.C.Types
@@ -20,44 +21,6 @@ import           XMonad.Layout.NoBorders
 import           XMonad.Layout.SimplestFloat
 import           XMonad.Layout.Spacing
 import qualified XMonad.StackSet              as W
-
-data Colours = Colours
-  { black        :: String
-  , red          :: String
-  , green        :: String
-  , yellow       :: String
-  , blue         :: String
-  , purple       :: String
-  , cyan         :: String
-  , white        :: String
-  , brightBlack  :: String
-  , brightRed    :: String
-  , brightGreen  :: String
-  , brightYellow :: String
-  , brightBlue   :: String
-  , brightPurple :: String
-  , brightCyan   :: String
-  , brightWhite  :: String
-  } deriving (Eq, Show)
-
-gruvbox = Colours
-  { black        = "#282828"
-  , red          = "#cc241d"
-  , green        = "#98971a"
-  , yellow       = "#d79921"
-  , blue         = "#458588"
-  , purple       = "#b16286"
-  , cyan         = "#689d6a"
-  , white        = "#a89984"
-  , brightBlack  = "#928374"
-  , brightRed    = "#fb4934"
-  , brightGreen  = "#b8bb26"
-  , brightYellow = "#fabd2f"
-  , brightBlue   = "#83a598"
-  , brightPurple = "#d3869b"
-  , brightCyan   = "#8ec07c"
-  , brightWhite  = "#ebdbb2"
-  }
 import           XMonad.Util.Run              (safeSpawn, spawnPipe)
 
 colours = gruvbox
