15a6a0eb7b88086892bdfee3f1f13ba9ad9b9706
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-10-29 13:49:26 +0100

    ghc: Update to 8.8.4

--- xmonad-splintah/default.nix
+++ xmonad-splintah/default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import ./nixpkgs.nix {}, compiler ? "ghc865" }:
+{ nixpkgs ? import ./nixpkgs.nix {}, compiler ? "ghc884" }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./xmonad-splintah.nix { }
--- xmonad-splintah/shell.nix
+++ xmonad-splintah/shell.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import ./nixpkgs.nix {}, compiler ? "ghc865" }:
+{ nixpkgs ? import ./nixpkgs.nix {}, compiler ? "ghc884" }:
 (nixpkgs.callPackage ./default.nix { inherit compiler; }).env
