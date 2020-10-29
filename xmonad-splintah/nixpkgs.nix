4f427a7ab86ca1bdd40a6e9915fdc1392d4a0991
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-10-29 13:49:04 +0100

    nix: Update Nixpkgs version to 20.09
    Use Niv to manage Nixpkgs dependency.

--- xmonad-splintah/nixpkgs.nix
+++ xmonad-splintah/nixpkgs.nix
@@ -1,6 +1,1 @@
-import (builtins.fetchGit {
-  name = "nixpkgs-unstable-2020-05-08";
-  url = https://github.com/nixos/nixpkgs-channels/;
-  ref = "refs/heads/nixpkgs-unstable";
-  rev = "5f14d99efed32721172a819b6e78a5520bab4bc6";
-})
+let sources = import ./nix/sources.nix; in import sources.nixpkgs
